f153ef5bf236e4c8160af5ed643e19deebfbde95
Bapan-git47 <bapanraina4747@gmail.com>
2023-12-02 21:07:46 +0530

    file added

--- Wordcounter.java
+++ Wordcounter.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a sentence or paragraph:");
+        String inputText = scanner.nextLine();
+
+        int wordCount = countWords(inputText);
+
+        System.out.println("Number of words: " + wordCount);
+    }
+
+    private static int countWords(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = text.split("\\s+");
+        return words.length;
+    }
+}
 
